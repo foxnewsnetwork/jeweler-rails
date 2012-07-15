2eb8678283007171fbdba59e7902227835be3b34
Thomas Chen <foxnewsnetwork@gmail.com>
2012-07-15 15:44:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JewelerRails::Application.config.secret_token = '2a9ed36d9b46400b2f8dede45e0ea972f85d667836c39a1840309f5ea6df2a5b3b19beeb39fea5a63fe9640a7e615f257ef96537432ecc43a4597523e5132ef6'
